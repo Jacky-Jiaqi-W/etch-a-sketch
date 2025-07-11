2b9715a3de2d75a04522df144f0553eb8b7cecac
Jiaqi Wang <yidaojin9@gmail.com>
2025-07-11 23:37:44 +0800

    Add the heading, the resize button and its functionality
    Separated the previous code for creating the 16x16 grid to be the
    createGrid function since it's also used when resizing the grid.

--- js-file.js
+++ js-file.js
@@ -1,19 +1,23 @@
 const container = document.querySelector("#container");
-container.style.display = "flex";
-container.style.flexDirection = "column";
-container.style.padding = "20px";
-for (let i = 1; i <= 16; i++) {
-    const rowContainer = document.createElement("div");
-    rowContainer.style.display = "flex";
-    rowContainer.style.justifyContent = "center";
-    for (let j = 1; j <= 16; j++) {
-        const squareDiv = document.createElement("div");
-        squareDiv.style.width = "40px";
-        squareDiv.style.height = "40px";
-        squareDiv.classList.add("square");
-        rowContainer.appendChild(squareDiv);
+const SQUARE_SIDE_LENGTH = 640;
+
+function createGrid(container, squaresPerSide, squareSideLength) {
+    container.style.display = "flex";
+    container.style.flexDirection = "column";
+    container.style.padding = "20px";
+    for (let i = 1; i <= squaresPerSide; i++) {
+        const rowContainer = document.createElement("div");
+        rowContainer.style.display = "flex";
+        rowContainer.style.justifyContent = "center";
+        for (let j = 1; j <= squaresPerSide; j++) {
+            const squareDiv = document.createElement("div");
+            squareDiv.style.width = `${squareSideLength/squaresPerSide}px`;
+            squareDiv.style.height = `${squareSideLength/squaresPerSide}px`;
+            squareDiv.classList.add("square");
+            rowContainer.appendChild(squareDiv);
+        }
+        container.appendChild(rowContainer);
     }
-    container.appendChild(rowContainer);
 }
 
 container.addEventListener("mouseover", e => {
@@ -21,3 +25,16 @@ container.addEventListener("mouseover", e => {
         e.target.classList.add("hover");
     }
 });
+
+const resizeBtn = document.querySelector("button");
+resizeBtn.addEventListener("click", e => {
+    let squaresPerSide = +prompt("Number of squares per side (1 - 100): ");
+    if (squaresPerSide < 1 || squaresPerSide > 100) {
+        alert("Invalid input number!")
+    } else {
+        container.innerHTML = "";
+        createGrid(container, squaresPerSide, SQUARE_SIDE_LENGTH);
+    }
+});
+
+createGrid(container, 16, SQUARE_SIDE_LENGTH);
