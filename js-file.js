a7788856a07d67b9b045ba2c0049106518f61126
Jiaqi Wang <yidaojin9@gmail.com>
2025-07-11 21:57:43 +0800

    Create the grid of square divs using the JS

---
+++ js-file.js
@@ -0,0 +1,18 @@
+const container = document.querySelector("#container");
+container.style.display = "flex";
+container.style.flexDirection = "column";
+container.style.gap = "20px";
+container.style.padding = "20px";
+for (let i = 1; i <= 16; i++) {
+    const rowContainer = document.createElement("div");
+    rowContainer.style.display = "flex";
+    rowContainer.style.gap = "20px";
+    for (let j = 1; j <= 16; j++) {
+        const squareDiv = document.createElement("div");
+        squareDiv.style.width = "40px";
+        squareDiv.style.height = "40px";
+        squareDiv.classList.add("square");
+        rowContainer.appendChild(squareDiv);
+    }
+    container.appendChild(rowContainer);
+}
